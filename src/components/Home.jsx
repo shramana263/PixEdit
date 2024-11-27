9983bb713805185202efea00bebc7944a9bd6059
shramana263 <shramana652@gmail.com>
2024-11-27 16:45:07 +0530

    first commit

---
+++ src/components/Home.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import './style/main.scss'
+
+const Home = () => {
+    return (
+        <div className='image_editor'>
+            <div className="card">
+                <div className="card_header">
+                    <h2>---- Image Editor ----</h2>
+                </div>
+                <div className="card_body">
+                    
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Home
