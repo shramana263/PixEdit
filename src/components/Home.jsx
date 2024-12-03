26096c2c721c2f1d7d47b340cc3d6afe887cb697
shramana263 <shramana652@gmail.com>
2024-12-03 23:07:05 +0530

    crop and style 03-12-2024

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -53,7 +53,17 @@ const Home = () => {
 
     const [details, setDetails] = useState('')
     const [count, setCount] = useState(1)
-    const [crop, setCrop] = useState('')
+    const [activeCrop, setActiveCrop] = useState(false)
+    const [crop, setCrop] = useState({
+
+        height: 100,
+        unit: "%",
+        width: 100,
+        x: 0,
+        y: 0
+
+    })
+    // const[crop, setCrop]= useState('')
 
     const [state, setState] = useState({
         image: '',
@@ -174,10 +184,12 @@ const Home = () => {
             const reader = new FileReader()
 
             reader.onload = () => {
+                // console.log(reader.result)
                 setState({
                     ...state,
                     image: reader.result
                 })
+                // console.log(state)
 
                 const stateData = {
                     image: reader.result,
@@ -204,6 +216,7 @@ const Home = () => {
     const handleCrop = () => {
         const canvas = document.createElement('canvas')
         // console.log(details.naturalWidth)
+        // console.log(crop.width)
         const scaleX = details.naturalWidth / details.width
         const scaleY = details.naturalHeight / details.height
 
@@ -228,6 +241,11 @@ const Home = () => {
         setState({
             ...state, image: base64Url
         })
+
+        const stateData = state
+        storeData.insert(stateData)
+
+        setActiveCrop(false)
     }
 
     const saveImage = () => {
@@ -291,7 +309,7 @@ const Home = () => {
                     <h2>~ Pix-Edit ~</h2>
                 </div>
                 <div className="card_body">
-                <div className="image_section">
+                    <div className="image_section">
                         <div className="image">
                             {/* {
                                 console.log(state.brightness)
@@ -300,21 +318,42 @@ const Home = () => {
                             {
                                 state.image ?
 
-                                    <ReactCrop crop={crop} onChange={c => setCrop(c)}>
-                                        <img
-                                            onLoad={(e) => setDetails(e.currentTarget)}
-                                            style={{
-                                                filter: `brightness(${state.brightness}%) 
+                                    <>
+                                        {
+                                            activeCrop &&
+                                            <ReactCrop crop={crop} onChange={c => { setCrop(c) }}>
+                                                <img
+                                                    onLoad={(e) => setDetails(e.currentTarget)}
+                                                    style={{
+                                                        filter: `brightness(${state.brightness}%) 
                                                 grayscale(${state.grayscale}%) 
                                                 sepia(${state.sepia}%) 
                                                 saturate(${state.saturate}%) 
                                                 contrast(${state.contrast}%) 
                                                 hue-rotate(${state.hueRotate}deg)`,
 
-                                                transform: `rotate(${state.rotate}deg) scale(${state.vertical}, ${state.horizontal})`
-                                            }}
-                                            src={state.image} alt="" />
-                                    </ReactCrop>
+                                                        transform: `rotate(${state.rotate}deg) scale(${state.vertical}, ${state.horizontal})`
+                                                    }}
+                                                    src={state.image} alt="" />
+                                            </ReactCrop>
+                                        }
+                                        {
+                                            !activeCrop &&
+                                            <img
+                                                onLoad={(e) => setDetails(e.currentTarget)}
+                                                style={{
+                                                    filter: `brightness(${state.brightness}%) 
+                                                grayscale(${state.grayscale}%) 
+                                                sepia(${state.sepia}%) 
+                                                saturate(${state.saturate}%) 
+                                                contrast(${state.contrast}%) 
+                                                hue-rotate(${state.hueRotate}deg)`,
+
+                                                    transform: `rotate(${state.rotate}deg) scale(${state.vertical}, ${state.horizontal})`
+                                                }}
+                                                src={state.image} alt="" />
+                                        }
+                                    </>
 
                                     :
 
@@ -329,8 +368,12 @@ const Home = () => {
                             <button className='undo' onClick={undo}><IoMdUndo /></button>
                             <button className='redo' onClick={redo}><IoMdRedo /></button>
                             {
-                                crop &&
-                                <button className='crop' onClick={handleCrop}>Crop Image</button>
+                                !activeCrop &&
+                                <button className='crop' onClick={()=>setActiveCrop(true)}>Crop Image</button>
+                            }
+                            {
+                                activeCrop &&
+                                <button className='crop' onClick={handleCrop}>Apply</button>
                             }
                             <label htmlFor="choose">Choose Image</label>
                             <input onChange={imageHandle} type="file" id='choose' />
@@ -369,20 +412,20 @@ const Home = () => {
                         </div>
 
                         <div className="reset">
-                            <button onClick={()=>setResetModalOpen(true)}>Reset</button>
+                            <button onClick={() => setResetModalOpen(true)}>Reset</button>
                             <button className='save'
                                 onClick={saveImage}
                             >Save Image</button>
                         </div>
                     </div>
 
-                   
+
                 </div>
             </div>
             {
                 isResetModalOpen &&
                 <div className='modal_container'>
-                    <Modal setResetModalOpen={setResetModalOpen} handleReset={handleReset}/>
+                    <Modal setResetModalOpen={setResetModalOpen} handleReset={handleReset} />
                 </div>
             }
         </div>
